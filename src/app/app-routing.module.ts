f3080a24ce4d64812bf90477b8e0bec575c6b788
Jesse <jessesouzaall@gmail.com>
2022-10-28 01:12:28 -0300

    finalizei os dados mockados de produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'produtos',
+    loadChildren: () =>
+      import('./produtos/produtos.module').then((m) => m.ProdutosModule),
+  },
+  { path: '', redirectTo: 'produtos', pathMatch: 'full' }, //aqui estou definindo a página produtos como a raiz
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
